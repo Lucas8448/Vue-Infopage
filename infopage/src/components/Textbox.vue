541cf92847fb5a367ed594a5500d408ccb5fb183
Lucas Bateson <lucas.bateson@gmail.com>
2022-11-10 09:24:20 +0100

    Clear old code

---
+++ infopage/src/components/Textbox.vue
@@ -0,0 +1,36 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="textbox">
+    <h1 class="title">{{ title }}</h1>
+    <p class="content">{{ content }}</p>
+  </div>
+</template>
+
+<style scoped>
+.textbox {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.content {
+  font-size: 1.5rem;
+  text-align: center;
+}
+
+</style>
--- infopage/src/views/Index.vue
+++ infopage/src/views/Index.vue
@@ -1,5 +1,6 @@
 <template>
   <h1 class="title">Online scams</h1>
+  
 </template>
 
 <style scoped>
